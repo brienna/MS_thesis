c6d7a904839969f1785499e5440d7d301d3bec0e
Brienna Herold <briennakh@gmail.com>
2020-08-17 18:17:47 -0500

    Add code to train doc2vec on RIT's supercomputer

---
+++ src/sporc_doc2vec_v1/helper.py
@@ -0,0 +1,90 @@
+import os
+import glob
+import sys
+import numpy as np
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from scipy.spatial.distance import cosine as cosine_distance
+
+FILE_DIR = os.path.dirname(os.path.abspath('__file__'))
+sys.path.append(FILE_DIR)
+
+def train_and_evaluate(triplets, p):
+    vector_size = p['vector_size']
+    dm = p['dm']
+    which_corpus = p['corpus']
+    datapath = os.path.join(FILE_DIR, 'abstracts/' + which_corpus + '/*.npy')
+    data = np.array(glob.glob(datapath))[:1000]
+    return {'score': Doc2VecModel(which_corpus, dm, vector_size).fit(data).evaluate(triplets),
+            'parameters': p}
+
+class Doc2VecModel(object): 
+    def __init__(self, which_corpus, dm=1, vector_size=100, window=1):
+        '''Must match all parameters in my param dict.'''
+        self.model = None
+        self.vector_size = vector_size
+        self.window = window
+        self.dm = dm
+        self.which_corpus = which_corpus
+
+    def fit(self, data):
+        self.model = Doc2Vec(vector_size=self.vector_size, 
+                             window=self.window, 
+                             dm=self.dm, 
+                             epochs=10,
+                             alpha=0.025, 
+                             min_alpha=0.001)
+        tagged_docs = self.DocumentIterator(data)
+        self.model.build_vocab(tagged_docs)
+        self.model.train(tagged_docs, total_examples=self.model.corpus_count, epochs=self.model.epochs)
+        return self
+
+    def get_vector(self, doc_id):
+        '''Takes a Pandas row for a paper.'''
+        loaded = np.load(os.path.join(FILE_DIR, 'abstracts/' + self.which_corpus + '/' + doc_id + '.npy'))
+        return self.model.infer_vector(loaded)
+
+    def evaluate(self, triplets):
+        '''Calculates averaged accuracy of all given triplets.
+        [accuracy that ab is more similar than ac, ab more similar than bc]'''
+        accuracies = [0, 0]
+        for triplet in triplets:
+            a_vector = self.get_vector(triplet[0])
+            b_vector = self.get_vector(triplet[1])
+            c_vector = self.get_vector(triplet[2])
+            ab_dist = cosine_distance(a_vector, b_vector)
+            ac_dist = cosine_distance(a_vector, c_vector)
+            bc_dist = cosine_distance(b_vector, c_vector)
+            if ab_dist > ac_dist and ab_dist > bc_dist:
+                accuracies[0] += 1
+                accuracies[1] += 1
+            elif ab_dist > ac_dist and ab_dist < bc_dist:
+                accuracies[0] += 1
+            elif ab_dist < ac_dist and ab_dist > bc_dist:
+                accuracies[1] += 1
+        return max([accuracies[0]/len(triplets), accuracies[1]/len(triplets)])
+
+    class DocumentIterator(object):
+        def load_documents(self):
+            for filename in self.filenames: 
+                loaded_file = np.load(filename)
+                tag = os.path.splitext(os.path.basename(filename))[0]
+                abstract = TaggedDocument(words=loaded_file, tags=[tag])
+                try:
+                    yield abstract
+                except Exception as e:
+                    print('Error!' + str(e))
+                finally:
+                    del loaded_file
+                    # print('Closed ' + tag)
+                    
+        def __init__(self, filenames):
+            self.filenames = filenames
+            #self.documents = self.load_documents()
+
+        def __iter__(self):
+            self.documents = self.load_documents() # Reset the iterator
+            return self
+        
+        def __next__(self):
+            abstract = next(self.documents)
+            return abstract
---
+++ src/sporc_doc2vec_v1/run_jobs.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -l
+
+# NOTE the -l flag! https://stackoverflow.com/questions/20499596/bash-shebang-option-l
+
+# This is an example job file for a multi-core MPI job.
+
+# Note that all of the following statements below that begin
+
+# with #SBATCH are actually commands to the SLURM scheduler
+
+# Please copy this file to your home directory and modify it 
+
+# to suit your needs.
+
+#
+
+# If you need any help,please email rc-help@rit.edu
+
+#
+
+# Name of the job.
+
+#SBATCH -J doc2vec_abstracts
+
+# Standard out and Standard Error output files
+
+#SBATCH -o doc2vec_abstracts.o
+
+#SBATCH -e doc2vec_abstracts.e
+
+# To send emails, set the address below and remove one of the '#' sings
+
+#SBATCH --mail-user=bkh4324@rit.edu
+
+# notify on state change: BEGIN, END, FAIL, OR ALL
+
+#SBATCH --mail-type=ALL
+
+# 5 days is the run time MAX, anything over will be KILLED unless you talk with RC
+
+# Request 4 days and 5 hours
+
+#SBATCH -t 3-5:0:0
+
+#Put the job in the appropriate partition matching the account and request cores (check parameter grid for how many)
+
+#SBATCH -A avxiv -p debug --ntasks=20
+
+#Job membory requirements in MB=m (default), GB=g, or TB=t
+
+#SBATCH --mem=100g
+
+######################################################################
+
+# Run 
+srun -n $SLURM_NTASKS --mpi=pmi2 python script.py 
+
+# IN TERMINAL NAVIGATE TO DIR W/ THIS .SH FILE & TYPE sbatch start_script.sh script.py
+# CHECK WHERE IT IS IN THE QUEUE WITH queue -u bkh4324
+# maybe cuz of the -l flag it won't work, need to check
+
+
