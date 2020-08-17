2ae67cc133d352075efdeb7e424c6243c6ef5935
Brienna Herold <briennakh@gmail.com>
2020-08-17 18:18:49 -0500

    Change ipyparallel to mpi4py

--- src/sporc_doc2vec_v1/script.py
+++ src/sporc_doc2vec_v1/script.py
@@ -1,75 +1,107 @@
-"""
-To run this script, open Terminal window and type: ipcluster start --profile=ipy_profile &
-Ensure that the profile name matches the one we're using below. 
-Wait until 
-Open another Terminal window, navigate to this directory, type: python script.py
-To stop the engine type in a third Terminal window: ipcluster stop --profile=ipy_profile
-Helpful resource: https://www.hyamani.eu/2018/05/20/parallel-super-computing-with-scikit-learn/
-Issues where I can't update a module once I've used it in the engine, I have to rename it or reset the engine
-"""
+# github.com/jbornschein/mpi4py-examples/blob/master/09-task-pull.py
 
-import os
-import sys
-import pickle
-import logging
-import argparse
+# mpirun -np 5 python script3.py
+
+# REQUIRED FILES IN ADDITION TO THIS FILE:
+    # triplets.npy 
+    # helper.py 
+    # abstracts/*.npy
+
+# SOME TERMINOLOGY: 
+
+# MPI world: All of the MPI processes, spawned by `mpirun` or SLURM/srun
+
+# MPI size: The number of MPI slots (or SLURM tasks)
+    # Within a SLURM job, this is the number of tasks (--ntasks) for the job.
+    # Outside of a SLURM job, this is the `-n` option given to `mpirun`. 
+
+# MPI rank: An integer number, in the range [0, MPI size], unique to each worker. 
+
+# This program has two parts: The controller and the worker part. 
+
+# The controller is executed by rank 0; the workers by everyone else.
+
+# We might have been able to use Scatterv if it accepted regular objects
+# comm.scatter() is not as convenient in practice cuz it requires the size 
+# of the large array to be divisible by the number of processes
+
+from mpi4py import MPI 
+import json
 import numpy as np
-from joblib import delayed, cpu_count
-from joblib import Parallel, parallel_backend, register_parallel_backend
-from ipyparallel import Client
-from ipyparallel.joblib import IPythonParallelBackend
 from sklearn.model_selection import ParameterGrid
-from timeit import default_timer as timer
-from datetime import timedelta
-from helper import train_and_evaluate # module in the same directory
+from helper import train_and_evaluate
 
-# Get current directory
-FILE_DIR = os.path.dirname(os.path.abspath('__file__'))
-sys.path.append(FILE_DIR)
+# Define MPI message tags
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
 
-# Prepare the logger
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--profile", 
-					default="ipy_profile", 
-					help="Name of IPython profile to use")
-args = parser.parse_args()
-profile = args.profile
-logging.basicConfig(filename=os.path.join(FILE_DIR,profile+'.log'),
-					filemode='w',
-					level=logging.DEBUG)
-print("number of CPUs found: {0}".format(cpu_count()))
-logging.info("number of CPUs found: {0}".format(cpu_count()))
-logging.info("args.profile: {0}".format(profile))
+def main():
+    # Initializations and preliminaries 
+    comm = MPI.COMM_WORLD           # get MPI communicator object
+    size = comm.size                # total number of processes
+    rank = comm.rank                # Rank of this process
+    status = MPI.Status()           # get MPI status object
+    scores = []
+    tags = enum('READY', 'DONE', 'EXIT', 'START')
+    triplets = np.load('100_triplets.npy')
 
+    if rank == 0: 
+        # Master process executes code below
+        params = {'dm': [0, 1],
+              'vector_size': [100, 200], 
+              'corpus': ['numtoken']}
+        tasks = list(ParameterGrid(params))
+        task_index = 0
+        num_workers = size - 1
+        closed_workers = 0
+        print('Master starting with %d workers' % num_workers)
 
-# Prepare the engines
-c = Client(profile=profile)
-# Make sure that each engine is running in the right 
-# working directory to access the custom function(s)
-c[:].map(os.chdir, [FILE_DIR]*len(c))
-logging.info("c.ids :{0}".format(str(c.ids)))
-bview = c.load_balanced_view()
-register_parallel_backend('ipyparallel', lambda : IPythonParallelBackend(view=bview))
+        while closed_workers < num_workers:
+            score = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            source = status.Get_source()
+            tag = status.Get_tag()
+            if tag == tags.READY:
+                # Worker is ready, so send it a task
+                if task_index < len(tasks):
+                    comm.send(tasks[task_index], dest=source, tag=tags.START)
+                    print('Sending task %d to worker %d' % (task_index, source))
+                    task_index += 1
+                else:
+                    comm.send(None, dest=source, tag=tags.EXIT)
+            elif tag == tags.DONE:
+                scores.append(score)
+                with open('scores.txt', 'w+') as scores_file: #seems like it is ok w full scores, rewrites?
+                    json.dump(scores, scores_file)
+                print('Got data from worker %d' % source)
+            elif tag == tags.EXIT:
+                print('Worker %d exited.' % source)
+                closed_workers += 1
 
-# Get evaluation triplets (we want just the filenames of the preprocessed documents)
-triplets = np.load('100_triplets.npy')
+            print('Master finishing')
+    else:
+        # Worker processes execute code below
+        name = MPI.Get_processor_name()
+        while True:
+            comm.send(None, dest=0, tag=tags.READY)
+            task = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+            tag = status.Get_tag()
+            if tag == tags.START:
+                # Do the work here
+                score = train_and_evaluate(triplets, task)
+                #score_formatted = {k: v for d in score for k, v in d.items()}
+                comm.send(score, dest=0, tag=tags.DONE)
+            elif tag == tags.EXIT:
+                break
 
-# Set parameters to test
-params = {
-    'dm': [0, 1],
-    'vector_size': [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
-    'corpus': ['numtoken'] 
-}
+        comm.send(None, dest=0, tag=tags.EXIT)
+    
 
-# Run the code
-start = timer()
-with parallel_backend('ipyparallel'):
-    scores = Parallel(n_jobs=len(c))(delayed(train_and_evaluate)(triplets, p, model_id) for model_id, p in enumerate(ParameterGrid(params)))
-    scores_formatted = {k: v for d in scores for k, v in d.items()}
-    with open('scores.pkl', 'wb') as f:
-        pickle.dump(scores_formatted, f, pickle.HIGHEST_PROTOCOL)
-end = timer()
-logging.info("Execution time HH:MM:SS: {0}".format(timedelta(seconds=end-start)))
+if __name__ == '__main__':
+    main()
+    
+    
+
 
 
 
