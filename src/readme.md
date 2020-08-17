8be852531f308a910105b82361ae96eced777c55
Brienna Herold <briennakh@gmail.com>
2020-08-17 18:14:55 -0500

    Set globals

--- src/readme.md
+++ src/readme.md
@@ -1,2 +1,6 @@
 - parse_xml — Older stand-alone code that parses XMLs using multiprocessing. I don't use this code anymore——most of it went into notebooks/Preprocess_corpus.ipynb——but preprocessing parts of it could be useful later on.
 
+- `set_globals.py` — Sets global variables to be used in multiple notebooks, generates `data/globals.p`.
+
+
+
---
+++ src/set_globals.py
@@ -0,0 +1,28 @@
+'''
+Sets absolute and relative filepaths.
+These filepaths are used by all code in this project.
+
+To generate pickled filepaths.p, run `python set_filepaths.py` 
+
+If running code on a different machine, e.g. the cloud, 
+consider pickling the new filepaths or using relative filepaths.
+'''
+
+import pickle
+
+root = '/Volumes/ARCHIVES/Thesis/'
+pickle_path = root + 'data/globals.p'
+
+absolute_filepaths = {'metadata': root + 'data/metadata.csv',
+  					  'conversion_log': root + 'data/conversion_log.csv',
+  					  'xml': root + 'data/xml/',
+  					  'archive': root + 'data/archive/',
+  					  'corpus': root + 'data/corpus/',
+  					  'data': root + 'data/',
+  					  'root': root}
+
+global_variables = {'absolute_filepaths': absolute_filepaths,
+					'start': 2009,
+					'end': 2019}
+
+pickle.dump(global_variables, open(pickle_path, 'wb'))
